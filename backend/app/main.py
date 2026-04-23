ec40cd612a88d2357af99df66ecfcb667ca03240
Utsav Kumar <utsav.udk@gmail.com>
2026-04-23 12:43:44 +0530

    fix Base override preventing table creation on render db
    Updated CORS middleware to allow requests from the Vercel app.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,26 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .database import engine, Base
 from . import models
 from .routes import auth_routes, tracking_routes
 from .routes.analytics_routes import router as analytics_router
-from fastapi.middleware.cors import CORSMiddleware
-from .database import engine
-from .models import Base
+
 
 app = FastAPI()
+
+# Create database tables
 Base.metadata.create_all(bind=engine)
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://fsd-product-analytics-dashboard.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-# Create tables automatically
-Base.metadata.create_all(bind=engine)
 app.include_router(auth_routes.router)
 app.include_router(tracking_routes.router)
 app.include_router(analytics_router)
@@ -28,4 +32,4 @@ app.include_router(analytics_router)
 
 @app.get("/")
 def read_root():
-    return {"message": "Vigility Dashboard Backend Running "}
+    return {"message": "Vigility Dashboard Backend Running"}
